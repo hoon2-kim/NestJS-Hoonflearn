6ba95c2d25de97ee6e6f6512e701541db4f9c05c
hoon2-kim <gooaba1204@gmail.com>
2023-10-27 17:21:44 +0900

    :white_check_mark: "Video - unit test 추가"

--- src/video/__tests__/video.controller.spec.ts
+++ src/video/__tests__/video.controller.spec.ts
@@ -1,20 +1,100 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideoController } from '@src/video/video.controller';
 import { VideoService } from '@src/video/video.service';
+import { mockCreatedInstructor } from '@test/__mocks__/user.mock';
+import { mockCreatedVideo, mockVideoService } from '@test/__mocks__/video.mock';
 
 describe('VideoController', () => {
-  let controller: VideoController;
+  let videoController: VideoController;
+  let videoService: VideoService;
+
+  const lessonId = 'uuid';
+  const user = mockCreatedInstructor;
+  const videoId = 'uuid';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VideoController],
-      providers: [VideoService],
+      providers: [
+        {
+          provide: VideoService,
+          useValue: mockVideoService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<VideoController>(VideoController);
+    videoController = module.get<VideoController>(VideoController);
+    videoService = module.get<VideoService>(VideoService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(videoController).toBeDefined();
+    expect(videoService).toBeDefined();
+  });
+
+  describe('[VideoController.uploadVideo] - 영상 업로드', () => {
+    const file = {
+      fieldname: 'video',
+      originalname: '영상.mp4',
+      encoding: '7bit',
+      mimetype: 'video/mp4',
+      buffer: Buffer.from('영상.mp4'),
+      size: 9902365,
+    } as Express.Multer.File;
+
+    it('영상 업로드 성공', async () => {
+      jest.spyOn(videoService, 'upload').mockResolvedValue(mockCreatedVideo);
+
+      const result = await videoController.uploadVideo(lessonId, file, user);
+
+      expect(result).toEqual(mockCreatedVideo);
+      expect(videoService.upload).toBeCalled();
+      expect(videoService.upload).toBeCalledWith(lessonId, file, user);
+    });
+
+    it('영상 업로드 실패 - 파일이 없는 경우(400에러)', async () => {
+      try {
+        await videoController.uploadVideo(lessonId, null, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+
+    it('영상 업로드 실패 - 파일이 limit사이즈를 초과한 경우(400에러)', async () => {
+      const bigFile = { ...file, size: 1024 * 1024 * 1024 * 5 };
+
+      try {
+        await videoController.uploadVideo(lessonId, bigFile, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+
+    it('영상 업로드 실패 - 영상 확장자가 mpg,mv4,mov,m2ts,mp4가 아닌 경우(400에러)', async () => {
+      const etcFile = { ...file, mimetype: 'video/avi' };
+
+      try {
+        await videoController.uploadVideo(lessonId, etcFile, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+  });
+
+  describe('[VideoController.deleteVideo] - 영상 업로드 삭제', () => {
+    it('영상 삭제 성공', async () => {
+      jest.spyOn(videoService, 'delete').mockResolvedValue(true);
+
+      const result = await videoController.deleteVideo(videoId, user);
+
+      expect(result).toBe(true);
+      expect(videoService.delete).toBeCalled();
+      expect(videoService.delete).toBeCalledWith(videoId, user);
+    });
   });
 });
--- src/video/__tests__/video.service.spec.ts
+++ src/video/__tests__/video.service.spec.ts
@@ -1,18 +1,356 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { VideoService } from '../video.service';
+import { DataSource, QueryRunner, Repository, UpdateResult } from 'typeorm';
+import { VideoService } from '@src/video/video.service';
+import { CourseService } from '@src/course/course.service';
+import { LessonService } from '@src/lesson/lesson.service';
+import { AwsS3Service } from '@src/aws-s3/aws-s3.service';
+import {
+  mockAwsS3Service,
+  mockCourseService,
+  mockCreatedVideo,
+  mockLessonService,
+  mockVideoWithLesson,
+} from '@test/__mocks__/video.mock';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { VideoEntity } from '@src/video/entities/video.entity';
+import { mockLessonWithVideo } from '@test/__mocks__/lesson.mock';
+import { mockCreatedSection } from '@test/__mocks__/section.mock';
+import { CourseEntity } from '@src/course/entities/course.entity';
+import { mockCreatedInstructor } from '@test/__mocks__/user.mock';
+import {
+  BadRequestException,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
+
+jest.mock('../../common/helpers/getVideoDuration.helper.ts', () => ({
+  getVideoDuration: jest.fn().mockResolvedValue(10),
+}));
+
+const mockQueryRunner = {
+  manager: {},
+} as QueryRunner;
+
+class MockDataSource {
+  createQueryRunner(): QueryRunner {
+    return mockQueryRunner;
+  }
+}
 
 describe('VideoService', () => {
-  let service: VideoService;
+  let videoService: VideoService;
+  let courseService: CourseService;
+  let lessonService: LessonService;
+  let awsS3Service: AwsS3Service;
+  let dataSource: DataSource;
+  let videoRepository: Repository<VideoEntity>;
+
+  const courseId = 'uuid';
+  const lessonId = 'uuid';
+  const videoId = 'uuid';
+  const user = mockCreatedInstructor;
+  const updateResult: UpdateResult = {
+    generatedMaps: [],
+    raw: [],
+    affected: 1,
+  };
 
   beforeEach(async () => {
+    Object.assign(mockQueryRunner.manager, {
+      save: jest.fn(),
+      findOne: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+      decrement: jest.fn(),
+    });
+
+    mockQueryRunner.connect = jest.fn();
+    mockQueryRunner.startTransaction = jest.fn();
+    mockQueryRunner.commitTransaction = jest.fn();
+    mockQueryRunner.rollbackTransaction = jest.fn();
+    mockQueryRunner.release = jest.fn();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VideoService],
+      providers: [
+        VideoService,
+        {
+          provide: CourseService,
+          useValue: mockCourseService,
+        },
+        { provide: LessonService, useValue: mockLessonService },
+        { provide: AwsS3Service, useValue: mockAwsS3Service },
+        { provide: DataSource, useClass: MockDataSource },
+        { provide: getRepositoryToken(VideoEntity), useValue: {} },
+      ],
     }).compile();
 
-    service = module.get<VideoService>(VideoService);
+    videoService = module.get<VideoService>(VideoService);
+    courseService = module.get<CourseService>(CourseService);
+    lessonService = module.get<LessonService>(LessonService);
+    awsS3Service = module.get<AwsS3Service>(AwsS3Service);
+    dataSource = module.get<DataSource>(DataSource);
+    videoRepository = module.get<Repository<VideoEntity>>(
+      getRepositoryToken(VideoEntity),
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(videoService).toBeDefined();
+    expect(courseService).toBeDefined();
+    expect(lessonService).toBeDefined();
+    expect(awsS3Service).toBeDefined();
+    expect(dataSource).toBeDefined();
+    expect(videoRepository).toBeDefined();
+  });
+
+  describe('[영상 업로드]', () => {
+    const file = {
+      fieldname: 'video',
+      originalname: '영상.mp4',
+      encoding: '7bit',
+      mimetype: 'video/mp4',
+      buffer: Buffer.from('영상.mp4'),
+      size: 9902365,
+    } as Express.Multer.File;
+
+    const uploadUrl = mockCreatedVideo.videoUrl;
+
+    const mockLessonWithOutVideo = { ...mockLessonWithVideo, video: null };
+
+    let queryRunner: QueryRunner;
+
+    beforeEach(() => {
+      queryRunner = dataSource.createQueryRunner();
+    });
+
+    it('영상 업로드 성공', async () => {
+      jest
+        .spyOn(lessonService, 'findOneByOptions')
+        .mockResolvedValue(mockLessonWithOutVideo);
+      jest
+        .spyOn(lessonService, 'getCourseIdByLessonIdWithQueryBuilder')
+        .mockResolvedValue(courseId);
+      jest
+        .spyOn(courseService, 'validateInstructor')
+        .mockResolvedValue(undefined);
+      jest.spyOn(awsS3Service, 'uploadFileToS3').mockResolvedValue(uploadUrl);
+      jest
+        .spyOn(queryRunner.manager, 'save')
+        .mockResolvedValue(mockCreatedVideo);
+      jest
+        .spyOn(queryRunner.manager, 'findOne')
+        .mockResolvedValueOnce(mockCreatedSection);
+      jest.spyOn(queryRunner.manager, 'update').mockResolvedValue(updateResult);
+      jest.spyOn(queryRunner.manager, 'findOne').mockResolvedValueOnce({
+        id: 'uuid',
+        totalVideosTime: 0,
+      } as CourseEntity);
+      jest.spyOn(queryRunner.manager, 'update').mockResolvedValue(updateResult);
+
+      const result = await videoService.upload(lessonId, file, user);
+
+      expect(result).toEqual(mockCreatedVideo);
+      expect(queryRunner.manager.save).toBeCalledTimes(1);
+      expect(queryRunner.manager.findOne).toBeCalledTimes(2);
+      expect(queryRunner.manager.update).toBeCalledTimes(2);
+      expect(queryRunner.commitTransaction).toBeCalledTimes(1);
+      expect(queryRunner.rollbackTransaction).toBeCalledTimes(0);
+      expect(queryRunner.release).toBeCalledTimes(1);
+    });
+
+    it('영상 업로드 실패 - 해당 수업이 없는 경우(404에러)', async () => {
+      jest.spyOn(lessonService, 'findOneByOptions').mockResolvedValue(null);
+
+      try {
+        await videoService.upload(lessonId, file, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(queryRunner.manager.save).toBeCalledTimes(0);
+        expect(queryRunner.commitTransaction).toBeCalledTimes(0);
+        expect(queryRunner.rollbackTransaction).toBeCalledTimes(1);
+        expect(queryRunner.release).toBeCalledTimes(1);
+      }
+    });
+
+    it('영상 업로드 실패 - 이미 업로드 한 경우(400에러)', async () => {
+      jest
+        .spyOn(lessonService, 'findOneByOptions')
+        .mockResolvedValue(mockLessonWithVideo);
+
+      try {
+        await videoService.upload(lessonId, file, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(queryRunner.manager.save).toBeCalledTimes(0);
+        expect(queryRunner.commitTransaction).toBeCalledTimes(0);
+        expect(queryRunner.rollbackTransaction).toBeCalledTimes(1);
+        expect(queryRunner.release).toBeCalledTimes(1);
+      }
+    });
+
+    it('영상 업로드 실패 - 해당 강의 만든 지식공유자가 아닌 경우(403에러)', async () => {
+      jest
+        .spyOn(lessonService, 'findOneByOptions')
+        .mockResolvedValue(mockLessonWithOutVideo);
+      jest
+        .spyOn(lessonService, 'getCourseIdByLessonIdWithQueryBuilder')
+        .mockResolvedValue(courseId);
+      jest
+        .spyOn(courseService, 'validateInstructor')
+        .mockRejectedValue(
+          new ForbiddenException('해당 강의를 만든 지식공유자가 아닙니다.'),
+        );
+
+      try {
+        await videoService.upload(lessonId, file, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(ForbiddenException);
+        expect(queryRunner.manager.save).toBeCalledTimes(0);
+        expect(queryRunner.commitTransaction).toBeCalledTimes(0);
+        expect(queryRunner.rollbackTransaction).toBeCalledTimes(1);
+        expect(queryRunner.release).toBeCalledTimes(1);
+      }
+    });
+
+    it('영상 업로드 실패 - S3에 업로드 실패하는 경우(400에러)', async () => {
+      jest
+        .spyOn(lessonService, 'findOneByOptions')
+        .mockResolvedValue(mockLessonWithOutVideo);
+      jest
+        .spyOn(lessonService, 'getCourseIdByLessonIdWithQueryBuilder')
+        .mockResolvedValue(courseId);
+      jest
+        .spyOn(courseService, 'validateInstructor')
+        .mockResolvedValue(undefined);
+      jest
+        .spyOn(awsS3Service, 'uploadFileToS3')
+        .mockRejectedValue(new BadRequestException());
+
+      try {
+        await videoService.upload(lessonId, file, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(queryRunner.manager.save).toBeCalledTimes(0);
+        expect(queryRunner.commitTransaction).toBeCalledTimes(0);
+        expect(queryRunner.rollbackTransaction).toBeCalledTimes(1);
+        expect(queryRunner.release).toBeCalledTimes(1);
+      }
+    });
+  });
+
+  describe('[업로드한 영상 삭제]', () => {
+    let queryRunner: QueryRunner;
+
+    beforeEach(() => {
+      queryRunner = dataSource.createQueryRunner();
+    });
+
+    it('영상 삭제 성공', async () => {
+      jest
+        .spyOn(queryRunner.manager, 'findOne')
+        .mockResolvedValueOnce(mockVideoWithLesson);
+      jest
+        .spyOn(lessonService, 'getCourseIdByLessonIdWithQueryBuilder')
+        .mockResolvedValue(courseId);
+      jest
+        .spyOn(courseService, 'validateInstructor')
+        .mockResolvedValue(undefined);
+      jest
+        .spyOn(awsS3Service, 'deleteS3Object')
+        .mockResolvedValue({ success: true });
+      jest
+        .spyOn(queryRunner.manager, 'delete')
+        .mockResolvedValue({ raw: [], affected: 1 });
+      jest
+        .spyOn(queryRunner.manager, 'findOne')
+        .mockResolvedValueOnce(mockCreatedSection);
+      jest
+        .spyOn(queryRunner.manager, 'decrement')
+        .mockResolvedValue(updateResult);
+      jest.spyOn(queryRunner.manager, 'findOne').mockResolvedValueOnce({
+        id: 'uuid',
+        totalVideosTime: 0,
+      } as CourseEntity);
+      jest
+        .spyOn(queryRunner.manager, 'decrement')
+        .mockResolvedValue(updateResult);
+
+      const result = await videoService.delete(videoId, user);
+
+      expect(result).toBe(true);
+      expect(queryRunner.manager.delete).toBeCalledTimes(1);
+      expect(queryRunner.manager.findOne).toBeCalledTimes(3);
+      expect(queryRunner.manager.decrement).toBeCalledTimes(2);
+      expect(queryRunner.commitTransaction).toBeCalledTimes(1);
+      expect(queryRunner.rollbackTransaction).toBeCalledTimes(0);
+      expect(queryRunner.release).toBeCalledTimes(1);
+    });
+
+    it('영상 업로드 실패 - 해당 영상이 없는 경우(404에러)', async () => {
+      jest.spyOn(queryRunner.manager, 'findOne').mockResolvedValue(null);
+
+      try {
+        await videoService.delete(lessonId, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(queryRunner.manager.save).toBeCalledTimes(0);
+        expect(queryRunner.commitTransaction).toBeCalledTimes(0);
+        expect(queryRunner.rollbackTransaction).toBeCalledTimes(1);
+        expect(queryRunner.release).toBeCalledTimes(1);
+      }
+    });
+
+    it('영상 업로드 실패 - 해당 강의를 만든 지식공유자가 아닌 경우(403에러)', async () => {
+      jest
+        .spyOn(queryRunner.manager, 'findOne')
+        .mockResolvedValue(mockVideoWithLesson);
+      jest
+        .spyOn(lessonService, 'getCourseIdByLessonIdWithQueryBuilder')
+        .mockResolvedValue(courseId);
+      jest
+        .spyOn(courseService, 'validateInstructor')
+        .mockRejectedValue(
+          new ForbiddenException('해당 강의를 만든 지식공유자가 아닙니다.'),
+        );
+
+      try {
+        await videoService.delete(lessonId, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(ForbiddenException);
+        expect(queryRunner.manager.save).toBeCalledTimes(0);
+        expect(queryRunner.commitTransaction).toBeCalledTimes(0);
+        expect(queryRunner.rollbackTransaction).toBeCalledTimes(1);
+        expect(queryRunner.release).toBeCalledTimes(1);
+      }
+    });
+
+    it('영상 업로드 실패 - S3에서 삭제 실패하는 경우(400에러)', async () => {
+      jest
+        .spyOn(queryRunner.manager, 'findOne')
+        .mockResolvedValue(mockVideoWithLesson);
+      jest
+        .spyOn(lessonService, 'getCourseIdByLessonIdWithQueryBuilder')
+        .mockResolvedValue(courseId);
+      jest
+        .spyOn(courseService, 'validateInstructor')
+        .mockResolvedValue(undefined);
+      jest
+        .spyOn(awsS3Service, 'deleteS3Object')
+        .mockRejectedValue(new BadRequestException());
+
+      try {
+        await videoService.delete(lessonId, user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(queryRunner.manager.save).toBeCalledTimes(0);
+        expect(queryRunner.commitTransaction).toBeCalledTimes(0);
+        expect(queryRunner.rollbackTransaction).toBeCalledTimes(1);
+        expect(queryRunner.release).toBeCalledTimes(1);
+      }
+    });
   });
 });
