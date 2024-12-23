3282e89f8d65f3ff17dfd803533be3001cfa5e13
hoon2-kim <gooaba1204@gmail.com>
2024-12-23 19:18:59 +0900

    :rocket: "프로젝트 재배포"

--- src/coupon/coupon.controller.ts
+++ src/coupon/coupon.controller.ts
@@ -6,20 +6,28 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from '@src/auth/decorators/current-user.decorator';
 import { Roles } from '@src/auth/decorators/role-protected.decorator';
 import { AtGuard } from '@src/auth/guards/at.guard';
 import { RoleGuard } from '@src/auth/guards/role.guard';
 import { ERoleType } from '@src/user/enums/user.enum';
 import { CouponService } from './coupon.service';
+import {
+  ApiCreateCouponSwagger,
+  ApiRegisterCouponSwagger,
+  ApiUpdateCouponSwagger,
+} from './coupon.swagger';
 import { CreateCouponDto } from './dtos/create-coupon.dto';
 import { RegisterCouponDto } from './dtos/register-coupon.dto';
 import { UpdateCouponDto } from './dtos/update-coupon.dto';
 
+@ApiTags('COUPON')
 @Controller('coupons')
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
 
+  @ApiCreateCouponSwagger('강사가 강의 쿠폰 생성')
   @Post('/instructors')
   @Roles(ERoleType.Instructor)
   @UseGuards(AtGuard, RoleGuard)
@@ -30,6 +38,7 @@ export class CouponController {
     return await this.couponService.createCoupon(createCouponDto, userId);
   }
 
+  @ApiRegisterCouponSwagger('유저가 강의 쿠폰 등록')
   @Post('/users')
   @UseGuards(AtGuard)
   async registerCoupon(
@@ -39,6 +48,7 @@ export class CouponController {
     return await this.couponService.registerCoupon(registerCouponDto, userId);
   }
 
+  @ApiUpdateCouponSwagger('강사가 쿠폰 수정')
   @Patch('/:id')
   @Roles(ERoleType.Instructor)
   @UseGuards(AtGuard, RoleGuard)
